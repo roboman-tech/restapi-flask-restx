4012e582654e87df6559d8848a6567c635276b1a
roboman-tech <roboman.tech@gmail.com>
2025-07-08 11:51:42 +0200

    feat: add crud functionalities

--- api/routes.py
+++ api/routes.py
@@ -48,3 +48,51 @@ class MovieResource(Resource):
         except Exception as e:
             print(f"Exception occurred: {e}")
             api.abort(500, message="Internal Server Error", type="error")
+
+    @api.response(404, 'Movie not Found')
+    @api.response(200, 'Success')
+    @api.response(500, 'Internal Server Error')
+    @api.expect(movie_model, code=200)
+    def put(self, id):
+        ''' Update a movie '''
+        try:
+            movie = Movies.query.get(id)
+            if movie:
+                payload = request.get_json()
+                movie.title = payload["title"]
+                movie.genre = payload["genre"]
+                movie.release_year = payload["release_year"]
+                db.session.commit()
+                return jsonify(movie.to_dict)
+            else:
+                return {"message": "The movie you are trying to update is not found", "type": "error"}, 404
+        except Exception as e:
+            db.session.rollback()
+            api.abort(500, f"Internal Server Error: {str(e)}")
+
+    @api.response(404, 'Movie not Found')
+    @api.response(200, 'Success')
+    @api.response(500, 'Internal Server Error')
+    def delete(self, id):
+        ''' Delete a movie'''
+        try:
+            movie_to_be_deleted = Movies.query.get(id)
+            if movie_to_be_deleted:
+                db.session.delete(movie_to_be_deleted)
+                db.session.commit()
+                return f'movie with id {id} has been deleted Successfully', 200
+            else:
+                return {"message": "The movie you are trying to delete is not found", "type": "error"}, 404
+        except Exception as e:
+            api.abort(500, f"Internal Server Error: {str(e)}")
+
+    @api.response(500, 'Internal Server Error')
+    @api.response(200, 'Success')
+    def get(self):
+        ''' Get all movies '''
+        try:
+            movies = Movies.query.all()
+            movies = [movie.to_dict for movie in movies]
+            return jsonify({"count": len(movies), "movies": movies})
+        except Exception as e:
+            api.abort(500, f"Internal Server Error: {str(e)}")
